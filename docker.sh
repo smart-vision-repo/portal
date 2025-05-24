f2d0232a315a2558454280f904f3bf05873e7360
tju <tony.ju@me.com>
2025-05-24 11:07:02 +0800

    update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,6 +6,8 @@ if [ $# -eq 0 ] || [ "$1" = "help" ] || [ "$1" = "-h" ]; then
     echo "Commands:"
     echo "  build    - Build Docker image for Smart Vision portal"
     echo "  run      - Run or restart the Smart Vision container"
+    echo "  stop     - Stop the Smart Vision container"
+    echo "  rm       - Delete the Smart Vision container"
     echo "  purge    - Remove Smart Vision container and image"
     echo "  help|-h  - Show this help message"
     exit 0
@@ -23,9 +25,9 @@ elif [ "$1" = "run" ]; then
     else
         # If doesn't exist, create new container
         if [ "$(uname)" = "Linux" ]; then
-            docker run -it --env-file /opt/bash/env -v /var/tmp/smart-vision:/var/tmp/smart-vision -v /opt/models/yolo:/opt/models/yolo -p 8501:8501 --gpus '"device=0"' smart-vision/portal
+            docker run -it --env-file /opt/bash/env -v /app:./ -v /var/tmp/smart-vision:/var/tmp/smart-vision -v /opt/models/yolo:/opt/models/yolo -p 8501:8501 --gpus '"device=0"' smart-vision/portal
         else
-            docker run -it --env-file /opt/bash/env -v /var/tmp/smart-vision:/var/tmp/smart-vision -v /opt/models/yolo:/opt/models/yolo -p 8501:8501 smart-vision-portal:
+            docker run -it --env-file /opt/bash/env -v /app:./ -v /var/tmp/smart-vision:/var/tmp/smart-vision -v /opt/models/yolo:/opt/models/yolo -p 8501:8501 smart-vision-portal:
         fi
     fi
     exit 0
@@ -34,4 +36,8 @@ elif [ "$1" = "build" ]; then
     docker rmi smart-vision-portal
     docker build -t smart-vision-portal .
     exit 0
+elif [ "$1" = "stop" ]; then
+    docker ps -a | grep smart-vision-portal | awk '{print "docker stop " $1}' | sh
+elif [ "$1" = "rm" ]; then
+    docker ps -a | grep smart-vision-portal | awk '{print "docker rm " $1}' | sh
 fi
